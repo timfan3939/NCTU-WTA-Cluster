3918e8d3fa6907f979fda3014f807c9bf6158fe4
C.T.Fan <timfan3939@gmail.com>
2017-11-25 11:16:24 +0000

    Central Command Behaviour is added.

--- src/tw/edu/nctu/dcslab/WTACluster/Agent/CentralAgent.java
+++ src/tw/edu/nctu/dcslab/WTACluster/Agent/CentralAgent.java
@@ -1,1 +1,21 @@
 package tw.edu.nctu.dcslab.WTACluster.Agent;
+
+import jade.core.Agent;
+import jade.core.behaviours.ThreadedBehaviourFactory;
+
+import tw.edu.nctu.dcslab.WTACluster.Agent.Behaviour.CentralCommandBehaviour;
+
+public class CentralAgent extends Agent {
+	private static final long serialVersionUID = -5271213701466983534L;
+	ThreadedBehaviourFactory tbf;
+
+	@Override
+	protected void setup() {
+		super.setup();
+
+		tbf = new ThreadedBehaviourFactory();
+		this.addBehaviour( tbf.wrap( new CentralCommandBehaviour(this) ) );
+
+	}
+
+}
