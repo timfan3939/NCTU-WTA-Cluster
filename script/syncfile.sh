2e378a746d195f50ece1cb09dd260a9b4fa01b60
C.T.Fan <timfan3939@gmail.com>
2017-11-26 11:00:40 +0000

    Update script file, it now can sync with config file

--- script/syncfile.sh
+++ script/syncfile.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-scp -r src/* weapon01:/home/timfan3939/NCTU-WTA-Cluster/src/
-scp build.xml weapon01:/home/timfan3939/NCTU-WTA-Cluster
-ssh weapon01 "cd NCTU-WTA-Cluster; ant clean build;"
+cat conf/peerList | while read name ip
+do
+	echo ${name}
+	echo "scp java files"
+	cmd="scp -r src/* ${name}:/home/timfan3939/NCTU-WTA-Cluster/src/"
+	eval $cmd
+	
+	echo ""
+	echo ""
+	echo "scp build.xml"
+	cmd="scp build.xml ${name}:/home/timfan3939/NCTU-WTA-Cluster/"
+	eval $cmd
+
+	echo ""
+	echo""
+	echo "building files"
+	cmd="ssh ${name} \"cd NCTU-WTA-Cluster; ant clean build;\""
+	eval $cmd
+done
+
+#scp -r src/* weapon01:/home/timfan3939/NCTU-WTA-Cluster/src/
+#scp build.xml weapon01:/home/timfan3939/NCTU-WTA-Cluster
+#ssh weapon01 "cd NCTU-WTA-Cluster; ant clean build;"
 
