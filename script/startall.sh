e1195e89b18eace384e3b884bd5ab7c390be502e
C.T.Fan <timfan3939@gmail.com>
2017-11-30 07:12:45 +0000

    Fix some bugs in the script, beautify the file

--- script/startall.sh
+++ script/startall.sh
@@ -2,10 +2,14 @@
 
 cat conf/peerList | while read -r name ip
 do
+	echo ""
+	echo "=========="
 	echo ${name}
-	echo "Starting..."
-	cmd="ssh -f ${name} \"ant -f NCTU-WTA-Cluster/build.xml weapon &>output.log \""
+	echo "=========="
+	echo "1. Starting agents on weapon"
+	cmd="ssh -f ${name} \"ant -f NCTU-WTA-Cluster/build.xml weapon \""
 	echo $cmd
 	eval $cmd
 	echo "...Done"
+	echo "----------"
 done
--- script/syncfile.sh
+++ script/syncfile.sh
@@ -2,14 +2,19 @@
 
 cat conf/peerList | while read -r name ip
 do
+	echo ""
+	echo "=========="
 	echo $name
-	echo "scp java files"
+	echo "=========="
+	echo "1. scp java files"
 	cmd="scp -r src/* ${name}:/home/timfan3939/NCTU-WTA-Cluster/src/"
 	eval $cmd
+	echo "...Done"
+	echo "----------"
 	
-	echo ""
-	echo ""
-	echo "scp build.xml"
+	echo "2. scp build.xml"
 	cmd="scp build.xml ${name}:/home/timfan3939/NCTU-WTA-Cluster/"
 	eval $cmd
+	echo "...Done"
+	echo "----------"
 done
