cfdbed98efa1ec87499b8325dec813febe7218be
C.T.Fan <timfan3939@gmail.com>
2017-12-20 08:39:17 +0000

    Compute now can parse the problem almost properly.

--- src/tw/edu/nctu/dcslab/WTACluster/Behaviour/ComputeExecuteBehaviour.java
+++ src/tw/edu/nctu/dcslab/WTACluster/Behaviour/ComputeExecuteBehaviour.java
@@ -1,5 +1,7 @@
 package tw.edu.nctu.dcslab.WTACluster.Behaviour;
 
+import java.util.ArrayList;
+
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
@@ -9,6 +11,7 @@ import tw.edu.nctu.dcslab.WTACluster.Problem.ProblemInterface;
 import tw.edu.nctu.dcslab.WTACluster.Problem.WTAProblem;
 import tw.edu.nctu.dcslab.WTACluster.Algorithm.HeuristicInterface;
 import tw.edu.nctu.dcslab.WTACluster.Algorithm.GeneticAlgorithm;
+import tw.edu.nctu.dcslab.WTACluster.util.PeerInfo;
 
 public class ComputeExecuteBehaviour extends Behaviour {
 	private static final long serialVersionUID = 20171219154629L;
@@ -20,7 +23,9 @@ public class ComputeExecuteBehaviour extends Behaviour {
 	private int state;
 
 	private String result;
-	private long starttime;
+	private long endtime;
+	private String problemID;
+	private ArrayList<PeerInfo> peerList;
 
 	private ProblemInterface problem;
 	private HeuristicInterface algorithm;
@@ -35,8 +40,10 @@ public class ComputeExecuteBehaviour extends Behaviour {
 		this.state = 0;
 		this.result = "";
 		this.problem = null;
-		this.starttime = 0;
+		this.endtime = 1000;
 		this.algorithm = null;
+		this.problemID = "";
+		this.peerList = new ArrayList<PeerInfo>();
 	}
 	
 	@Override
@@ -65,9 +72,9 @@ public class ComputeExecuteBehaviour extends Behaviour {
 
 		for(String str:subContent) {
 			String[] subLine = str.split("\n");
-
-			this.result += "" + subLine.length + " ";
 		}
+		
+		this.problemID = subContent[0].split("\n")[1].trim();
 
 		if ( subContent[1].startsWith("WTAProblem:") ) {
 			this.problem = new WTAProblem(1, 1);
@@ -75,7 +82,32 @@ public class ComputeExecuteBehaviour extends Behaviour {
 			this.algorithm = new GeneticAlgorithm(this.problem, 100);
 		}
 
-		this.starttime = System.currentTimeMillis();
+		for ( String line : subContent[2].split("\n") ) {
+			if( line.isEmpty() )
+				continue;
+			String[] subLine = line.split(" ");
+			if( subLine[0].matches(this.myAgent.getLocalName())) {
+				continue;
+			}
+			this.peerList.add( new PeerInfo( subLine[0], subLine[1] ) );
+		}
+
+		result += "\nPeers: " + this.peerList.size();
+
+		for( String line:subContent[3].split("\n") ) {
+			String[] subLine = line.split(":");
+			switch( subLine[0] ){
+				case "Time":
+					this.endtime = Long.parseLong(subLine[1]);
+					break;
+				default:
+					break;
+			}
+		}
+
+
+
+		this.endtime += System.currentTimeMillis();
 		this.iterCount = 0;
 
 
@@ -85,9 +117,9 @@ public class ComputeExecuteBehaviour extends Behaviour {
 	}
 
 	private void execution() {
-		if ( System.currentTimeMillis() - this.starttime > 10000 ) {
+		if ( System.currentTimeMillis() > this.endtime ) {
 			this.state = 2;
-			result += " " + this.problem.fitnessFunction(this.algorithm.GetBestSolution());
+			result += "\n" + this.problem.fitnessFunction(this.algorithm.GetBestSolution());
 			return;
 		}
 		for( int i=0; i<10; i++) {
@@ -102,7 +134,7 @@ public class ComputeExecuteBehaviour extends Behaviour {
 
 		ACLMessage reply = message.createReply();
 		reply.setPerformative(ACLMessage.CONFIRM);
-		reply.setContent("Result from " + myAgent.getName() + " of Problem " + "this.problemID" + " " + this.result);
+		reply.setContent("Result from " + myAgent.getName() + " of Problem " + this.problemID + " " + this.result);
 		this.myAgent.send( reply );
 
 		this.state = 3;
