b5680910f43c088919bb9154a212d06948d98ba8
C.T.Fan <timfan3939@gmail.com>
2018-02-27 04:00:26 +0000

    Fix trailing whitespace

--- script/gcloudGen.py
+++ script/gcloudGen.py
@@ -12,6 +12,6 @@ for i in range(lower, upper+1):
 print( 'gcloud compute disks create {} --zone asia-east1-a --source-snapshot snapshot-1711221 --type pd-standard'.format(strdisk))
 
 for i in range(lower, upper+1):
-	strvm +=  ' gcloud compute instances create "weapon{0:0>2}" --zone "asia-east1-a" --disk name=weapon{0:0>2},device-name=instance-1,mode=rw,boot=yes;'.format(i) 
+	strvm +=  ' gcloud compute instances create "weapon{0:0>2}" --zone "asia-east1-a" --disk name=weapon{0:0>2},device-name=instance-1,mode=rw,boot=yes;'.format(i)
 
 print( strvm )
