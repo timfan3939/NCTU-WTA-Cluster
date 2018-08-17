7b1f2322afb979fc1ae7c924d74f25bd6550c07e
Chih-Tien Fan <timfan3939@gmail.com>
2018-08-17 03:55:35 +0000

    Update gcloud command generator

--- script/gcloudGen.py
+++ script/gcloudGen.py
@@ -2,6 +2,9 @@
 
 lower = int(input('lowerbound:'))
 upper = int(input('upperbound:'))
+vmtype = int(input('vm Type? 1) n1-standard, 2) f1-micro :') )
+
+vmtypes = ['', 'n1-standard-1', 'f1-micro']
 
 strdisk = ""
 strvm = ""
@@ -9,9 +12,9 @@ strvm = ""
 for i in range(lower, upper+1):
 	strdisk += ' "weapon{0:0>2}"'.format(i)
 
-print( 'gcloud compute disks create {} --zone asia-east1-a --source-snapshot snapshot-1711221 --type pd-standard'.format(strdisk))
+print( 'gcloud compute disks create {} --zone asia-east1-a --source-snapshot snapshot-180817 --type pd-standard'.format(strdisk))
 
 for i in range(lower, upper+1):
-	strvm +=  ' gcloud compute instances create "weapon{0:0>2}" --zone "asia-east1-a" --disk name=weapon{0:0>2},device-name=instance-1,mode=rw,boot=yes;'.format(i)
+	strvm +=  'gcloud compute instances create "weapon{0:0>2}" --zone "asia-east1-a" --disk name=weapon{0:0>2},device-name=instance-1,mode=rw,boot=yes --machine-type={1}; '.format(i, vmtypes[vmtype])
 
 print( strvm )
